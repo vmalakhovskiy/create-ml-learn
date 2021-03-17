6217eaf9f5cbe50eed6527bd4766fad82c6307e8
Dieter Kunze <42535710+dietskunze@users.noreply.github.com>
2021-03-17 14:40:14 +0200

    Input -> MLMultiArray

--- ActivityRecorder/ActivityHelper.swift
+++ ActivityRecorder/ActivityHelper.swift
@@ -6,13 +6,24 @@
 //
 
 import Foundation
+import CoreML
 
 class ActivityHelper {
     
     func isSquatting(input: Input) -> Bool {
-        let classifier = WorkoutActivityClassifier()
-        //
-        return false //CHECK
+        let activityClassificationModel = WorkoutActivityClassifier()
+        var currentIndexInPredictionWindow = 0
+
+        let accelDataX = try! MLMultiArray(shape: [input.accX] as [NSNumber], dataType: MLMultiArrayDataType.double)
+        let accelDataY = try! MLMultiArray(shape: [input.accY] as [NSNumber], dataType: MLMultiArrayDataType.double)
+        let accelDataZ = try! MLMultiArray(shape: [input.accZ] as [NSNumber], dataType: MLMultiArrayDataType.double)
+
+        let gyroDataX = try! MLMultiArray(shape: [input.rotX] as [NSNumber], dataType: MLMultiArrayDataType.double)
+        let gyroDataY = try! MLMultiArray(shape: [input.rotY] as [NSNumber], dataType: MLMultiArrayDataType.double)
+        let gyroDataZ = try! MLMultiArray(shape: [input.rotZ] as [NSNumber], dataType: MLMultiArrayDataType.double)
+
+        var stateOutput = try! MLMultiArray(shape:[input.stateIn as NSNumber], dataType: MLMultiArrayDataType.double)
+        return false
     }
     
 }
