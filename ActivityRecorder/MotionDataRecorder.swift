de24060e166e5c4ce6575a64eb054439509f07f7
Vitalii Malakhovskyi <vitalii.malakhovskyi@gen.tech>
2021-03-17 15:13:45 +0200

    added prediction

--- ActivityRecorder/MotionDataRecorder.swift
+++ ActivityRecorder/MotionDataRecorder.swift
@@ -1,42 +1,84 @@
 import Foundation
 import CoreMotion
+import CoreML
 let sensorsUpdateInterval = 1.0 / 50.0
 
 struct MotionData {
-    let accelDataX: Double
-    let accelDataY: Double
-    let accelDataZ: Double
-    let gyroDataX: Double
-    let gyroDataY: Double
-    let gyroDataZ: Double
+    let accelDataX: MLMultiArray
+    let accelDataY: MLMultiArray
+    let accelDataZ: MLMultiArray
+    let rotationDataX: MLMultiArray
+    let rotationDataY: MLMultiArray
+    let rotationDataZ: MLMultiArray
+    let stateOutput: MLMultiArray
 }
 
 class MotionDataRecorder {
+    
+    struct ModelConstants {
+        static let predictionWindowSize = 30
+        static let sensorsUpdateInterval = 1.0 / 50.0
+        static let stateInLength = 400
+    }
+    
     let motionManager = CMMotionManager()
     let motionDataQueue = OperationQueue()
-   var recoring = false
+    var recoring = false
+    
+    init() {
+        motionManager.accelerometerUpdateInterval = TimeInterval(ModelConstants.sensorsUpdateInterval)
+        motionManager.gyroUpdateInterval = TimeInterval(ModelConstants.sensorsUpdateInterval)
+        
+    }
     
     func startMotionUpdates(handler: @escaping (MotionData) -> ()) {
         if motionManager.isDeviceMotionAvailable {
             recoring = true
+            
+            var currentIndexInPredictionWindow = 0
+            
+            let accelDataX = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
+            let accelDataY = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
+            let accelDataZ = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
+            
+            let rotationDataX = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
+            let rotationDataY = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
+            let rotationDataZ = try! MLMultiArray(shape: [ModelConstants.predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
+            
+            var stateOutput = try! MLMultiArray(shape:[ModelConstants.stateInLength as NSNumber], dataType: MLMultiArrayDataType.double)
+            
             self.motionManager.deviceMotionUpdateInterval = sensorsUpdateInterval
             
             self.motionManager.startDeviceMotionUpdates(
                 to: self.motionDataQueue, withHandler: { (data, error) in
                     if let validData = data {
                         let userAcceleration = validData.userAcceleration
-                        let gyroData = validData.rotationRate
+                        let rotationData = validData.rotationRate
                         
-                        let motionData = MotionData(
-                            accelDataX: userAcceleration.x,
-                            accelDataY: userAcceleration.y,
-                            accelDataZ: userAcceleration.z,
-                            gyroDataX: gyroData.x,
-                            gyroDataY: gyroData.y,
-                            gyroDataZ: gyroData.z
-                        )
-
-                        handler(motionData)
+                        accelDataX[[currentIndexInPredictionWindow] as [NSNumber]] = userAcceleration.x as NSNumber
+                        accelDataY[[currentIndexInPredictionWindow] as [NSNumber]] = userAcceleration.y as NSNumber
+                        accelDataZ[[currentIndexInPredictionWindow] as [NSNumber]] = userAcceleration.z as NSNumber
+                        
+                        rotationDataX[[currentIndexInPredictionWindow] as [NSNumber]] = rotationData.x as NSNumber
+                        rotationDataY[[currentIndexInPredictionWindow] as [NSNumber]] = rotationData.y as NSNumber
+                        rotationDataZ[[currentIndexInPredictionWindow] as [NSNumber]] = rotationData.z as NSNumber
+                        
+                        currentIndexInPredictionWindow += 1
+                        
+                        if (currentIndexInPredictionWindow == ModelConstants.predictionWindowSize) {
+                            handler(
+                                MotionData(
+                                    accelDataX: accelDataX,
+                                    accelDataY: accelDataY,
+                                    accelDataZ: accelDataZ,
+                                    rotationDataX: rotationDataX,
+                                    rotationDataY: rotationDataY,
+                                    rotationDataZ: rotationDataZ,
+                                    stateOutput: stateOutput
+                                )
+                            )
+                            currentIndexInPredictionWindow = 0
+                        }
                     }
                 })
         }
--- ActivityRecorder/ViewController.swift
+++ ActivityRecorder/ViewController.swift
@@ -7,7 +7,7 @@ class ViewController: UIViewController {
     @IBOutlet var stopRecordButton: UIButton!
 
     let motionDataRecorder = MotionDataRecorder()
-//    let model = WorkoutActivityClassifier()
+    let model = try! WorkoutActivityClassifier(configuration: MLModelConfiguration())
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +20,25 @@ class ViewController: UIViewController {
             motionDataRecorder.stopMotionUpdates()
             startRecordButton.titleLabel?.text = "begin squating"
         } else {
-            motionDataRecorder.startMotionUpdates { data in
-                print(data)
+            motionDataRecorder.startMotionUpdates { [weak self] data in
+                guard let strongSelf = self else { return }
+                print()
+                
+                let modelPrediction = try! strongSelf.model.prediction(
+                    acceleration_x: data.accelDataX,
+                    acceleration_y: data.accelDataY,
+                    acceleration_z: data.accelDataZ,
+                    rotation_x: data.rotationDataX,
+                    rotation_y: data.rotationDataY,
+                    rotation_z: data.rotationDataZ,
+                    stateIn: data.stateOutput
+                )
+                
+                //shsould be returned to MotionDataRecorder
+                let output = modelPrediction.stateOut
+                
+                print(modelPrediction.label)
+
             }
             startRecordButton.titleLabel?.text = "stop squatting"
         }
